8e58cdc7704e73bd37b653f8015ee1860c65db2a
Chase <Chase@104.39.250.209>
2017-11-03 17:20:12 -0400

    works again

--- src/LoginViewController.java
+++ src/LoginViewController.java
@@ -1,30 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-
-/**
- * FXML Controller class
- *
- * @author Nate Cox
- */
-public class LoginViewController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
-    public void displayPage() {
-        
-    }
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+ 
+public class LoginViewController {
+    @FXML private Text actiontarget;
     
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+
 }
