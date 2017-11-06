8781636e6d00f2d81bda5640fc0c8aae01b28e39
hanson <hanson@104.39.15.79>
2017-11-05 20:36:01 -0500

    add method on main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,11 +15,22 @@
  *
  * @author Nate Cox
  */
-public class Main {
+public class Main extends Application {
 
     /**
      * @param args the command line arguments
      */
+        @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        
+        Scene scene = new Scene(root);
+        String login = "LoginView.login";
+        scene.getStylesheets().add(login);
+        stage.setScene(scene);
+        
+        stage.show();
+    }
     public static void main(String[] args) {
         
         NavController.launch(NavController.class);
